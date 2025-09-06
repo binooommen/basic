1560b27650246c9ca67e6e804ea7363b859e1273
Bino Oommen Samuel <binooommen37@gmail.com>
2025-09-05 22:59:02 -0400

    Added webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_API_BASE': JSON.stringify(process.env.REACT_APP_API_BASE || 'http://localhost:4000/api'),
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:4000',
+      },
+    ],
+  },
+  mode: 'development',
+};
